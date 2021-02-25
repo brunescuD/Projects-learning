6d084233f976929b0cad12fe6d63b45ae671d508
Daniel Brunescu <bdanok@gmail.com>
2021-02-25 10:32:26 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-package schoolManagementSystem;
-
-public class Main {
-    public static void main(String[] args) {
-        School lmk = new School();
-        System.out.println(lmk.getStudents());
-    }
-}
